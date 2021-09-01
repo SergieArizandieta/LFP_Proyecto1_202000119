5b67c3a350a036a1e2ec08de63a8bae01a1c6936
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-08-31 20:06:30 -0600

    Limpieza

--- Operaciones.py
+++ Operaciones.py
@@ -18,47 +18,21 @@ def openExtra():
     else:
         texto = archivo.read()
         archivo.close()
-        #print('\n"Lectura exitosa"')
+        print('\n"Lectura exitosa"')
         return texto
 
-#Purificacion de los datos
+#Obtiene la cadena de texto
 def purificacionExtra():
-
-
     text = openExtra()
-    titulo = ""
-    textTemp = ""
-
     print(text)
 
-
+#Crea la tabla de tokens
 def TablaTokens():
-    TokensReservados = ["TITULO","ANCHO","ALTO","FILAS","COLUMNAS","CELDAS","FILTROS"]
-    ReservadosPatron = [["(T)(I)(T)(U)(L)(O)","(A)(N)(C)(H)(O)","(A)(L)(T)(O)","(F)(I)(L)(A)(S)","(C)(O)(L)(U)(M)(N)(A)(S)","(C)(E)(L)(D)(A)(S)","(F)(I)(L)(T)(R)(O)(S)"]]
-    Tokens = [["Titulo","Numero","Boolean","Color","Filtro"]]
-    TokensPatron = [["L={a-z,A-Z} L+","D={0-9} D+","((T)(R)(U)(E))|((F)(A)(L)(S)(E))","#DDDDDDD","(M)(I)(R)(R)(O)(R)(X)|(M)(I)(R)(R)(O)(R)(Y)|(D)(O)(U)(B)(L)(E)(M)(I)(R)(R)(O)(R)"]]
 
     AllPatrones = ["(T)(I)(T)(U)(L)(O)","(A)(N)(C)(H)(O)","(A)(L)(T)(O)","(F)(I)(L)(A)(S)","(C)(O)(L)(U)(M)(N)(A)(S)","(C)(E)(L)(D)(A)(S)","(F)(I)(L)(T)(R)(O)(S)","L={a-z,A-Z} L+","D={0-9} D+","((T)(R)(U)(E))|((F)(A)(L)(S)(E))","#DDDDDDD","(M)(I)(R)(R)(O)(R)(X)|(M)(I)(R)(R)(O)(R)(Y)|(D)(O)(U)(B)(L)(E)(M)(I)(R)(R)(O)(R)"]
     AllTokens= ["TITULO","ANCHO","ALTO","FILAS","COLUMNAS","CELDAS","FILTROS","Titulo","Numero","Boolean","Color","Filtro"]
     listaTokens= []
     
-    """ for lista in TokensReservados:
-        #print(lista)
-        listaTokens.append(lista)"""
-                        
-    print("///////////////////////////////")
-
-    """for lista in ReservadosPatron:
-        #print(lista)
-        listaTokens.append(lista)"""
-
-    print("///////////////////////////////")
-    for lista in listaTokens:
-        print(lista)
-         
-    print("///////////////////////////////")
-    #print(listaTokens)
-    #print(AllTokens[0])
 
     for i in range (0,12):
         aux = []
@@ -67,10 +41,11 @@ def TablaTokens():
         aux.append(AllTokens[i])
         aux.append(AllPatrones[i])
         listaTokens.append(aux)
-     
         #print(aux)
 
-    print(listaTokens)
+    #print(listaTokens)
+    print(listaTokens[0])
+    print(listaTokens[0][0])
 
 
 
--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from Operaciones import *
 if __name__ == "__main__":
     print("Hola")
-    #purificacionExtra()
+    purificacionExtra()
     TablaTokens()
