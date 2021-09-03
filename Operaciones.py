f651f7f4ca6f5475a228b9cb81975bf2f319aaf3
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-09-02 22:54:32 -0600

    Liempieza de codigo

--- Operaciones.py
+++ Operaciones.py
@@ -1,6 +1,7 @@
 from tkinter import filedialog, Tk
 
-TablaDeTokens = []
+AllPatrones = []
+AllTokens = []
 
 def openExtra():
     Tk().withdraw()
@@ -30,30 +31,39 @@ def purificacionExtra():
     EntradaEtiqueta = ""
     for txt in text:
         #print(txt)
-        if txt != " " and Entrada == False:
+        if txt != " " and Entrada == False and txt != "\n" and txt != "@":
             
-            if txt == "=":
+            if txt == "=" :
                 Entrada = True
+   
+
+                for Tokens in AllTokens:
+                    if Etiquetas.__eq__(Tokens): 
+                        print(Etiquetas)
+                        Etiquetas = ""
+                        break
+                
             else:
                 Etiquetas+=txt
 
 
         if (txt != " " and Entrada == True) or (txt == "\n") :
-            if txt != "=":
+            if txt != "=" :
                 if txt == ";":
                     Entrada = False
                 else:
                     EntradaEtiqueta+=txt
                 
-           
-    print(Etiquetas)
     print('\n\n\nOtro')
-    print(EntradaEtiqueta)
+    #print(Etiquetas)
+    print('\n\n\nOtro')
+    #print(EntradaEtiqueta)
 
 #Crea la tabla de tokens
 def TablaTokens():
-
+    global AllPatrones
     AllPatrones = ["(T)(I)(T)(U)(L)(O)","(A)(N)(C)(H)(O)","(A)(L)(T)(O)","(F)(I)(L)(A)(S)","(C)(O)(L)(U)(M)(N)(A)(S)","(C)(E)(L)(D)(A)(S)","(F)(I)(L)(T)(R)(O)(S)","L={a-z,A-Z} L+","D={0-9} D+","((T)(R)(U)(E))|((F)(A)(L)(S)(E))","#DDDDDDD","(M)(I)(R)(R)(O)(R)(X)|(M)(I)(R)(R)(O)(R)(Y)|(D)(O)(U)(B)(L)(E)(M)(I)(R)(R)(O)(R)"]
+    global AllTokens
     AllTokens= ["TITULO","ANCHO","ALTO","FILAS","COLUMNAS","CELDAS","FILTROS","Titulo","Numero","Boolean","Color","Filtro"]
     listaTokens= []
     
