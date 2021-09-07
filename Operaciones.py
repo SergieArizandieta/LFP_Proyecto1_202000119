fab7d5d6f6650fb5cb00178a88bcb7a7b359102c
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-09-07 17:37:57 -0600

    Implementando estados 3,4,5,6,7

--- Operaciones.py
+++ Operaciones.py
@@ -47,13 +47,15 @@ def purificacionExtra():
                 elif isNumero(txt):
                     txtTemp += txt
                     estado =4
+                    opcion = False
 
                 elif ord(txt) == 35: # #
                     txtTemp += txt
                     estado = 5
+                    opcion = False
 
                 elif isSimbolo(txt): 
-                    txtTemp += txt
+                    txtTemp += txt    
                     estado = 2
 
                 elif ord(txt) == 34: # "
@@ -70,12 +72,16 @@ def purificacionExtra():
                     if ord(txt) == 32 or ord(txt) == 10 or ord(txt) == 9 or txt == '~':
                         opcion = False
                         pass
-                    else: #@
-                        print("Error Lexico, se detecto " + txt + " en S0 F,C")
+                    else: 
+                        print("Error Lexico, se detecto " + txt + " en S0  F: " + str(fila) + ", C: " + str(columna))
 
             elif estado == 1:
                 opcion = False
                 if (isLetra(txt)):
+                    txtTemp += txt
+                    estado = 1
+                
+                elif (ord(txt) == 95 ): # _
                     txtTemp += txt
                     estado = 1
                 
@@ -90,29 +96,109 @@ def purificacionExtra():
                     opcion = True
 
             elif estado == 2:
-                print("Se reconocio en S2: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna ))
+                if len(txtTemp)>1:
+                    print("Se reconocio en S2: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
+                    if(txtTemp == "@@@@"):
+                        opcion = False
+                    else:
+                        opcion = True
+                else:
+                    print("Se reconocio en S2: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna ))
+                    opcion = False
                 txtTemp = ""
                 estado = 0
-                opcion = False
-
+            
             elif estado == 3:
-                
                 if ord(txt) != 34:
                     txtTemp += txt
                     opcion = False
                 else:
                     txtTemp += txt
-                    print("Se reconocio en S2: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
+                    estado = 2
+                    opcion = False
+                    
+                 
+            elif estado == 4:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 4
+
+                else:
+                    print("Se reconocio en S1: '" + txtTemp + "' F: " + str(fila) + ", C: " + str(columna - len(txtTemp)))
                     txtTemp = ""
                     estado = 0
-                    opcion = False
-              
+                    opcion = True
 
+            elif estado == 5:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 7
+
+            elif estado == 7:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 9
+            
+            elif estado == 9:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 11
+
+            elif estado == 11:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 12
+            
+            elif estado == 12:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 13
+
+            elif estado == 13:
+                opcion = False
+           
+                if (isNumero(txt)):
+                    txtTemp += txt
+                    estado = 2
+
+            elif estado == 6:
+                opcion = False
+           
+                if (ord(txt) == 64):
+                    txtTemp += txt
+                    estado = 8
+
+            elif estado == 8:
+                opcion = False
+           
+                if (ord(txt) == 64):
+                    txtTemp += txt
+                    estado = 10
+            
+            elif estado == 10:
+                opcion = False
+           
+                if (ord(txt) == 64):
+                    txtTemp += txt
+                    estado = 2
+                
 
         # Control de filas y columnas
         # Salto de Linea
         if (ord(txt) == 10):
-            columna = 0
+            columna = 1
             fila += 1
             continue
         # Tab Horizontal
@@ -137,7 +223,7 @@ def isLetra(txt):
 
 def isSimbolo(txt):
     
-    if(ord(txt) == 61 or ord(txt) == 59 or ord(txt) == 123 or ord(txt) == 125  or ord(txt) == 91 or ord(txt) == 93):
+    if(ord(txt) == 61 or ord(txt) == 59 or ord(txt) == 123 or ord(txt) == 125  or ord(txt) == 91 or ord(txt) == 93 or ord(txt) == 44):
         return True
     else:
         return False
